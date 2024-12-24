19d4d5649c59e2aec0b2ce2bf248e86c0dc5c942
Abdulhadi-assi <178458269+Abdulhadi-Assi@users.noreply.github.com>
2024-12-24 16:02:42 +0300

    adding create order to controller

--- src/main/java/food_delivery/controller/OrderController.java
+++ src/main/java/food_delivery/controller/OrderController.java
@@ -1,5 +1,12 @@
 package food_delivery.controller;
 
+import food_delivery.mapper.OrderMapper;
+import food_delivery.model.Order;
+import food_delivery.request.OrderRequest;
+import food_delivery.service.OrderService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +18,16 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("api/v1/order")
 @RestController
 public class OrderController {
-	
+
+    private final OrderService orderService;
+
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createOrder(
+            @RequestBody OrderRequest orderRequest)
+    {
+        Order order = orderService.createOrder(orderRequest.getCustomerId(), orderRequest.getAddressId());
+        return ResponseEntity.ok(OrderMapper.toDto(order));
+    }
 	
 }
